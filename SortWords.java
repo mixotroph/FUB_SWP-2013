2b083de05473411e370d0d90f54397d0ebdf191a
Chris vHF <c.vhf@fu-berlin.de>
2013-03-04 21:51:51 +0100

    SortWords.java: Task completed

--- SortWords.java
+++ SortWords.java
@@ -2,28 +2,53 @@
  * FUB Softwarepraktikum 2013
  * Uebung 1, Aufgabe 1
  *
+ * Idee: Alle Wörter der Datei werden gelesen, mit ihrer Länge markiert.
+ * und  in eine Prioritätswarteschlange eingefügt; Die Länge des Wortes 
+ * dient dabei als Schlüssel. Anschließend wird die Reihenfolge durch 
+ * einen Stack invertiert.
+ *
  * @author Christoph van Heteren-Frese
  * @version 1.0
  */
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class SortWords {
-  /**
-   * reads file
-   *
-   */
-  private static void ReadFile(String file) {
+  // Innere Klasse um die Worte mit ihrer Länge zu markieren
+  static class ComparableEntry implements Comparable<ComparableEntry> {
+    // Instanzvariablen
+    Integer key;
+    String value;
+    // Constructor
+    public ComparableEntry(Integer k, String v) { key=k; value=v;}
+    public Integer getKey() {return key;}
+    public String getValue() {return value;}
+
+    public int compareTo(ComparableEntry e) {
+      if (this.getKey()==e.getKey()) return 0;
+      else if (this.getKey()<e.getKey()) return -1;
+      else return 1;
+    }
+  }
+
+  public static void main (String[] args) throws IOException {
+    // get file name 
+    String file = args[0];
     Reader reader = null;
+    // initialize priority queue and stack
+    Stack<String> s = new Stack<String>();
+    PriorityQueue<ComparableEntry> pq;
+    pq = new PriorityQueue<ComparableEntry>();  
     try
     {
       reader = new FileReader(file);
       Scanner sc = new Scanner(reader);
       while(sc.hasNext()){
-        // just for testing, has to be replaced with 
-        // appropiate data structure (e.g. array list)
-        System.out.println(sc.next());
+        String next = sc.next();
+        pq.add(new ComparableEntry(new Integer(next.length()),next));
       }
     }
     catch ( IOException e ) {
@@ -32,20 +57,9 @@ public class SortWords {
     finally {
       try { reader.close(); } catch ( Exception e ) { }
     }
-  }
-  /**
-   * sort file
-   *
-   */
-  private static void SortFile() {
-
-  }
-
-  /**
-   * main method for testing 
-   */
-  public static void main (String[] args) throws IOException {
-    ReadFile("test_file");
+    // as long as pq is not empty get next element and push it on s
+    while(pq.size()>0) s.push(pq.poll().getValue());
+    // as long as s is not empty pop elements an write to console
+    while(!(s.empty())) System.out.println(s.pop());
   }
 }
-
