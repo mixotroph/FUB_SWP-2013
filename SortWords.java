825e6fb14da8c4edd5446c26bfcd6404a7d9e725
Chris vHF <c.vhf@fu-berlin.de>
2013-03-04 18:46:25 +0100

    add file test_file; modified SortWords.java: add function ReadFile

--- SortWords.java
+++ SortWords.java
@@ -6,12 +6,46 @@
  * @version 1.0
  */
 
-import java.util.List;
+import java.io.*;
+import java.util.Scanner;
 
 public class SortWords {
   /**
+   * reads file
+   *
+   */
+  private static void ReadFile(String file) {
+    Reader reader = null;
+    try
+    {
+      reader = new FileReader(file);
+      Scanner sc = new Scanner(reader);
+      while(sc.hasNext()){
+        // just for testing, has to be replaced with 
+        // appropiate data structure (e.g. array list)
+        System.out.println(sc.next());
+      }
+    }
+    catch ( IOException e ) {
+      System.err.println( "Fehler beim Lesen der Datei!" );
+    }
+    finally {
+      try { reader.close(); } catch ( Exception e ) { }
+    }
+  }
+  /**
+   * sort file
+   *
+   */
+  private static void SortFile() {
+
+  }
+
+  /**
    * main method for testing 
    */
-  public static void main (String[] args) {
+  public static void main (String[] args) throws IOException {
+    ReadFile("test_file");
   }
 }
+
