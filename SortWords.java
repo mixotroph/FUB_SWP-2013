c12527a154027ad86726f595e96302184d5fd1e6
Chris vHF <c.vhf@fu-berlin.de>
2013-03-04 12:13:44 +0100

    Aufgabe 1: added very first draft of SortWords.java

---
+++ SortWords.java
@@ -0,0 +1,17 @@
+/**
+ * FUB Softwarepraktikum 2013
+ * Uebung 1, Aufgabe 1
+ *
+ * @author Christoph van Heteren-Frese
+ * @version 1.0
+ */
+
+import java.util.List;
+
+public class SortWords {
+  /**
+   * main method for testing 
+   */
+  public static void main (String[] args) {
+  }
+}
